b1e377efa6dd88d365ed53bc3897b0792453d0c8
hieuvm <hieuvm28@gmail.com>
2026-09-16 22:40:50 +0700

    Them class health_metric

---
+++ healthmate/lib/models/health_metric.dart
@@ -0,0 +1,13 @@
+class HealthMetric {
+  double weight;
+  double height;
+  int heartRate;
+  int steps;
+
+  HealthMetric({
+    required this.weight,
+    required this.height,
+    required this.heartRate,
+    required this.steps,
+  });
+}
